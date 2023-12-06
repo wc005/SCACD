f8ce644aa58c7c984c1bfc19149142eae02be96e
wc005 <67750302+wc005@users.noreply.github.com>
2023-12-06 09:12:02 +0800

    Update initModel.py

--- initModel.py
+++ initModel.py
@@ -13,11 +13,9 @@ def initSCACD(window, args):
     encoder_input_dim = args.z_size
     encoder_hid_dim = args.hidden_size
     decoder_hidden_dim = encoder_hid_dim
-    # 先验 Z 概率
-    prior = Prior.autoencoder(input_size, args.hidden_size, args.z_size)
-    # RNN 漂移网络
-    ShiftModel = CausaConv.CausaConv(encoder_input_dim, encoder_hid_dim, args.n_layers, args.dropout)
-    # 解码器
+    
+    prior = Prior.autoencoder(input_size, args.hidden_size, args.z_size)    
+    ShiftModel = CausaConv.CausaConv(encoder_input_dim, encoder_hid_dim, args.n_layers, args.dropout)    
     decoder = Decoder.Decoder(args.z_size, decoder_hidden_dim, args.z_samples, args.s_samples, window)
     model = SCACD.SCACD(prior, ShiftModel, decoder)
     return model
